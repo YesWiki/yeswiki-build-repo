ffad78b5d973c4900c56749e553c05c44d9e5420
Florian Schmitt <mrflos@gmail.com>
2021-07-30 12:58:48 +0300

    feat(yunohost) : md5sum to sha256sum

--- app/YunoHost.php
+++ app/YunoHost.php
@@ -1,14 +1,18 @@
 <?php
+
 namespace YesWikiRepo;
 
-class YunoHost {
+class YunoHost
+{
     public $repository;
 
-    public function __construct(Repository $repository) {
+    public function __construct(Repository $repository)
+    {
         $this->repository = $repository;
     }
 
-    public function update() {
+    public function update()
+    {
         if (!empty($ynhDir = $this->getGitFolder())) {
             $subRepoName = $this->repository->localConf["yunohost-subRepo"];
 
@@ -57,7 +61,8 @@ class YunoHost {
         }
     }
 
-    private function getGitFolder() {
+    private function getGitFolder()
+    {
         if (!empty($this->repository->localConf["yunohost-git"])) {
             $repository = $this->repository->localConf["yunohost-git"];
             $branch = $this->repository->localConf["yunohost-git-source-branch"];
@@ -80,58 +85,67 @@ class YunoHost {
         }
     }
 
-    private function getManifest($ynhDir) {
+    private function getManifest($ynhDir)
+    {
         $manifest = new JsonFile($ynhDir . '/manifest.json');
         $manifest->read();
         return $manifest;
     }
 
-    private function getYnhLoginLDAPVersion($ynhDir) {
+    private function getYnhLoginLDAPVersion($ynhDir)
+    {
         $commonshContents = file_get_contents($ynhDir.'/scripts/_common.sh');
         preg_match('/loginldap_version="([0-9-]+)"/', $commonshContents, $matches);
         return $matches[1];
     }
 
-    private function replaceInFile($file, $search, $replace) {
+    private function replaceInFile($file, $search, $replace)
+    {
         $contents = file_get_contents($file);
         $contents = str_replace($search, $replace, $contents);
-        if (file_put_contents($file, $contents) === false ) {
+        if (file_put_contents($file, $contents) === false) {
             throw new \Exception("Error writing file : " . $file, 1);
         }
     }
 
-    private function updateLogin($ynhDir, $ynhLoginVersion, $currentLoginVersion) {
+    private function updateLogin($ynhDir, $ynhLoginVersion, $currentLoginVersion)
+    {
         $this->replaceInFile($ynhDir.'/scripts/_common.sh', $ynhLoginVersion, $currentLoginVersion);
     }
 
-    private function updateReadmes($ynhDir, $ynhVersion, $currentYWVersion) {
+    private function updateReadmes($ynhDir, $ynhVersion, $currentYWVersion)
+    {
         $this->replaceInFile($ynhDir.'/README.md', $ynhVersion, $currentYWVersion);
         $this->replaceInFile($ynhDir.'/README_fr.md', $ynhVersion, $currentYWVersion);
     }
 
-    private function updateAppSrc($ynhDir, $subRepoName, $ynhVersion, $currentYWVersion) {
-        $md5File = $this->repository->localConf['repo-path'] . $subRepoName . '/yeswiki-' . $subRepoName . '-' . $currentYWVersion . '.zip.md5';
-        $md5 = explode(' ', file_get_contents($md5File))[0];
+    private function updateAppSrc($ynhDir, $subRepoName, $ynhVersion, $currentYWVersion)
+    {
+        exec('curl -SsL ' . $this->repository->localConf['repo-path'] . $subRepoName . '/yeswiki-' . $subRepoName . '-' . $currentYWVersion . '.zip | sha256sum -', $sha);
+        $sha = $sha[0];
 
         $appSrcContents = file_get_contents($ynhDir.'/conf/app.src');
         $appSrcContents = str_replace($ynhVersion, $currentYWVersion, $appSrcContents);
-        $appSrcContents = preg_replace('/SOURCE_SUM=[0-9a-f]+/i', 'SOURCE_SUM='.$md5, $appSrcContents);
-        if (file_put_contents($ynhDir.'/conf/app.src', $appSrcContents) === false ) {
+        $appSrcContents = str_replace('SOURCE_SUM_PRG=md5sum', 'SOURCE_SUM_PRG=sha256sum', $appSrcContents);
+        $appSrcContents = preg_replace('/SOURCE_SUM=[0-9a-f]+/i', 'SOURCE_SUM='.$sha, $appSrcContents);
+        if (file_put_contents($ynhDir.'/conf/app.src', $appSrcContents) === false) {
             throw new \Exception("Error writing file : " . $ynhDir.'/conf/app.src', 1);
         }
     }
 
-    private function updateCheckProcess($ynhDir) {
+    private function updateCheckProcess($ynhDir)
+    {
         $masterHash = exec('cd '.$ynhDir.'; git rev-parse yunohost/master');
 
         $checkProcessContents = file_get_contents($ynhDir.'/check_process');
         $checkProcessContents = preg_replace('/commit=[0-9a-f]+/i', 'commit='.$masterHash, $checkProcessContents);
-        if (file_put_contents($ynhDir.'/check_process', $checkProcessContents) === false ) {
+        if (file_put_contents($ynhDir.'/check_process', $checkProcessContents) === false) {
             throw new \Exception("Error writing file : " . $ynhDir.'/check_process', 1);
         }
     }
 
-    private function checkoutBranch($ynhDir, $branchName, $from) {
+    private function checkoutBranch($ynhDir, $branchName, $from)
+    {
         $branches = exec('cd '.$ynhDir.'; git branch -l');
         if (!str_contains($branches, $branchName)) {
             echo exec('cd '.$ynhDir.'; git checkout --force -b '.$branchName.' '.$from)."\n";
@@ -140,11 +154,13 @@ class YunoHost {
         }
     }
 
-    private function commit($ynhDir, $message) {
+    private function commit($ynhDir, $message)
+    {
         echo exec('cd '.$ynhDir.'; git commit -a -m "'.$message.'"')."\n";
     }
 
-    private function push($ynhDir, $branchName) {
+    private function push($ynhDir, $branchName)
+    {
         echo exec('cd '.$ynhDir.'; git push repository '.$branchName)."\n";
     }
 }
